facaa53e480eb76c767637f104ea34ef9255fd29
Henrik Petersson <44243358+hnrkcode@users.noreply.github.com>
2022-08-24 15:28:43 +0200

    Movie variables into settings module

--- loading_api_wrapper/api.py
+++ loading_api_wrapper/api.py
@@ -2,18 +2,13 @@ import math
 
 import requests
 
-API_URL = "https://api.loading.se"
-API_VERSION = "v1"
-USER_AGENT = "Mozilla/5.0 (Windows NT 10.0; rv:91.0) Gecko/20100101 Firefox/91.0"
-EDITORIAL_POST_TYPES = [
-    "neRegular",
-    "review",
-    "opinion",
-    "update",
-    "podcast",
-    "conversation",
-]
-EDITORIAL_SORT = ["title"]
+from loading_api_wrapper.settings import (
+    API_URL,
+    API_VERSION,
+    EDITORIAL_POST_TYPES,
+    EDITORIAL_SORT,
+    USER_AGENT,
+)
 
 
 class LoadingApiWrapper:

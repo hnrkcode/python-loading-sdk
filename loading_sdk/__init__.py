874ca5693e3e3ed10c2b4f5576627c078b0bf00d
Henrik Petersson <44243358+hnrkcode@users.noreply.github.com>
2022-09-02 19:36:06 +0200

    Add support for asynchronous code

--- loading_sdk/__init__.py
+++ loading_sdk/__init__.py
@@ -1,3 +1,4 @@
 from loading_sdk.api import LoadingApiClient
+from loading_sdk.async_api import async_loading_api_client as AsyncLoadingApiClient
 
-__all__ = ["LoadingApiClient"]
+__all__ = ["LoadingApiClient", "AsyncLoadingApiClient"]
